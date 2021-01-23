0b7e559e30e8264926aa0f5733300a19095c2bfb
@jitao123 <1432599195@qq.com>
2021-01-23 15:28:59 +0800

    mybatis-plus逆向生成代码的基础工程

---
+++ src/test/java/com/myself/mybatisplusgeneratecode/MybatisplusgeneratecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.mybatisplusgeneratecode;
+
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.enums.ApiErrorCode;
+import com.sun.tools.javac.main.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplusgeneratecodeApplicationTests {
+
+
+}
